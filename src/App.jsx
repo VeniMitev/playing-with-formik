bb1ab5dec9933aac93c15f537535bc625d785cd4
VeniMitev <97586359+VeniMitev@users.noreply.github.com>
2022-05-17 01:14:15 -0700

    Refractoring to make form a separate component

--- src/App.jsx
+++ src/App.jsx
@@ -1,59 +1,12 @@
 import React from "react";
-import './index.css'
-import { useFormik } from 'formik';
+import './index.css';
+import Form from './components/Form.tsx';
 
 function App() {
 
-  const formik = useFormik({
-    initialValues: {
-      email: '',
-      password: ''
-    },
-
-    onSubmit: values => {
-      console.log(values);
-      alert('Login Succesful')
-    },
-
-    validate: values => {
-      let errors = {
-        email: '',
-        password: ''
-      };
-
-      if (!values.email) {
-        errors.email = 'Field Required';
-      } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)) {
-        errors.email = 'Username should be an email';
-      }
-
-      if(!values.password) errors.password = "Field Required";
-      return errors
-    }
-  });
-
   return (
-    <div>
-      <div className="login-form">
-        <h1>LOGIN</h1>
-        <form>
-          <h3>Email</h3>
-          <input id='email' type="text" onChange={formik.handleChange} value={formik.values.email} />
-            <div>
-              {formik.errors.email ? <div style={{color: 'red'}}>{formik.errors.email}</div> : <div></div>}
-            </div>
-            
-          <h3>Password</h3>
-          <input id='password' type="password" onChange={formik.handleChange} value={formik.values.password} />
-            <div>
-              {formik.errors.password ? <div style={{color: 'red'}}>{formik.errors.password}</div> : <div></div>}
-            </div>
-            
-          <div>
-            <button id="submitBtn" type='submit'><h3>Submit</h3></button>
-          </div>
-        </form>
-      </div>
+    <div className="App">
+      <Form />
     </div>
   );
 }
---
+++ src/components/Form.tsx
@@ -0,0 +1,59 @@
+import React from "react";
+import './Form.css'
+import { useFormik } from 'formik';
+
+function Form() {
+
+    const formik = useFormik({
+      initialValues: {
+        email: '',
+        password: ''
+      },
+  
+      onSubmit: values => {
+        console.log(values);
+        alert("Login Succesful");
+      },
+  
+      validate: values => {
+        let errors = {
+          email: '',
+          password: ''
+        };
+  
+        if (!values.email) {
+          errors.email = 'Field Required';
+        } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)) {
+          errors.email = 'Username should be an email';
+        }
+  
+        if(!values.password) errors.password = "Field Required";
+        return errors
+      }
+    });
+  
+    return (
+      <div>
+        <form className="login-form" onSubmit={formik.handleSubmit}>
+            <h1>LOGIN</h1>
+            
+            <h3>Email</h3>
+            <input id='email' type="text" onChange={formik.handleChange} value={formik.values.email} />
+                <div>
+                {formik.errors.email ? <div style={{color: 'red'}}>{formik.errors.email}</div> : <div></div>}
+                </div>
+                
+            <h3>Password</h3>
+            <input id='password' type="password" onChange={formik.handleChange} value={formik.values.password} />
+                <div>
+                {formik.errors.password ? <div style={{color: 'red'}}>{formik.errors.password}</div> : <div></div>}
+                </div>
+        
+            <button id="submitBtn" type="submit"><h3>Submit</h3></button>
+        </form>
+      </div>
+    );
+  }
+  
+  export default Form;
+  
